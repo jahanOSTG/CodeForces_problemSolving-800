80a4c1cbc628072c927592d47c1c5a26b8e68665
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-08 09:32:56 +0600

    Add files via upload

---
+++ Tram/Tram.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+int tram=0,minimum=0;
+cin>>n;
+for(int i=1;i<n;i++){
+    int a,b;
+
+        cin>>a>>b;
+        tram=tram-a;
+        tram=tram+b;
+        if(tram>minimum){
+
+            minimum=tram;
+        }
+
+
+}
+cout<<minimum<<endl;
+
+
+
+
+
+
+
+
+
+}
