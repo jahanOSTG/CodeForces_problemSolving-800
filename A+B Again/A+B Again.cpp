224ce30440b9050b6c4b0e1ab008027200bb8f4f
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-10 23:51:45 +0600

    Add files via upload

---
+++ A+B Again/A+B Again.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, t;
+    int sum;
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+        cin >> n;
+        int a = n / 10;
+        int b = n % 10;
+        sum = a + b;
+        cout << sum << endl;
+    }
+}
