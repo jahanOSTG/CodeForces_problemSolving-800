e4acf431ac63e7757805cccf4ed2f59673376f79
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-10 20:25:42 +0600

    Add files via upload

---
+++ Holiday Of Equality/Holiday Of Equality.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    int n;
+    int a[100];
+    cin>>n;
+    int max=0,sum=0;
+    for(int i=1; i<=n; i++){
+        cin>>a[i];
+          if(a[i]>max){
+        max=a[i];
+    }
+    sum=sum+a[i];
+    }
+    int result=0;
+
+    for(int i=1; i<=n; i++){
+        result=result+(max-a[i]);
+    }
+    cout<<result;
+
+}
+
