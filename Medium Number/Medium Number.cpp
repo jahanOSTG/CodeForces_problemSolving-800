aa48f7a9537f2ec5d9917b6d19d118d1ce3e8c3e
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-11 10:37:26 +0600

    Add files via upload

---
+++ Medium Number/Medium Number.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c;
+        if((a>b && a<c) || (a<b && a>c)){
+            cout<<a<<endl;
+        }
+        else if((b>a && b<c) || (b<a && b>c)){
+            cout<<b<<endl;
+        }
+        else{
+            cout<<c<<endl;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
