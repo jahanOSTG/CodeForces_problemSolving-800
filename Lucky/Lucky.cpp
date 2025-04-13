0da5b95ad91afc9c80b0956fd50400dded7172d2
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-13 12:20:54 +0600

    Update Lucky.cpp

--- Lucky/Lucky.cpp
+++ Lucky/Lucky.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 int main(){
-    int ticket[100];
     int t;
     cin >> t;
 
     for(int i = 0; i < t; i++){
-        for(int j = 0; j < 6; j++){
-            cin >> ticket[j];
-        }
-        int sum1 = ticket[0] + ticket[1] + ticket[2];
-        int sum2 = ticket[3] + ticket[4] + ticket[5];
+        string ticket;
+        cin >> ticket;
+
+        int sum1 = (ticket[0] - '0') + (ticket[1] - '0') + (ticket[2] - '0');
+        int sum2 = (ticket[3] - '0') + (ticket[4] - '0') + (ticket[5] - '0');
 
         if(sum1 == sum2){
             cout << "YES" << endl;
