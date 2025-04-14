2f0e6669b8bcb7c6422c0136bd0f9334424badb9
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-15 00:00:06 +0600

    Add files via upload

---
+++ Again Twenty Five/Again Twenty Five.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    long long count=1;
+    if(n<=2){
+    for(int i=1;i<=n;i++){
+            count=count*5;
+
+    }
+    cout<<count;
+    }
+}
