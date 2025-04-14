b4d080634acb0dd16dd90e3484aceac3d5a8f540
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-14 20:19:03 +0600

    Add files via upload

---
+++ Marathon/Marathon.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    for(int i = 1; i <= t; i++){
+        int a[4];
+        int count = 0;
+
+        for(int j = 0; j < 4; j++){
+            cin >> a[j];
+        }
+
+        if(a[1] > a[0]) count++;
+        if(a[2] > a[0]) count++;
+        if(a[3] > a[0]) count++;
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
