7e0a629159ee373e06749b398953cb08d068b4dc
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-15 01:05:38 +0600

    Add files via upload

---
+++ Restoring Three Numbers/Restoring Three Numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int x[4];
+    int a,b,c;
+    for(int i=0;i<4;i++){
+        cin>>x[i];
+    }
+    sort(x,x+4);
+    a=x[3]-x[1];
+    b=x[3]-x[2];
+    c=x[3]-x[0];
+     cout << a << " " << b << " " << c << endl;
+
+}
