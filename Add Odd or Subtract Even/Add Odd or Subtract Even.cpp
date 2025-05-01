635dccdead0add90ed8af19b7a61f2123ac3bb07
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-01 23:37:19 +0600

    Add files via upload

---
+++ Add Odd or Subtract Even/Add Odd or Subtract Even.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x,y;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>x>>y;
+        if(x==y){
+            cout<<"0"<<endl;
+        }else if(x<y){
+            if(abs(x-y)%2==0){
+                cout<<"2"<<endl;
+            }else{
+                 cout<<"1"<<endl;
+            }
+        }else{
+            if((x-y)%2==0){
+                cout<<"1"<<endl;
+            }else{
+                cout<<"2"<<endl;
+            }
+        }
+    }
+}
