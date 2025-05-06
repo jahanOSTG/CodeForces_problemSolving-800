c6e49895ce2177faaf04527c5d7d70779232e4f8
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-06 22:41:05 +0600

    Add files via upload

---
+++ Trippi Troppi/Trippi Troppi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    string a;
+    string b;
+    string c;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c;
+
+        cout<<a[0]<<b[0]<<c[0]<<endl;
+    }
+}
