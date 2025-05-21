08533df868ef8d5037303a190f4854c7f685d14f
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-22 00:50:00 +0600

    Add files via upload

---
+++ Calculating Function/Calculating Function.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    long long a;
+    cin>>a;
+    if(a%2==0){
+        cout<<a/2<<endl;
+    }else{
+        long long b=(a+1)/2;
+        cout<<-b<<endl;
+    }
+
+
+
+
+
+
+
+
+    }
+
