f1aac239e6aac61270499198b269ab19bf905054
Faria Jahan Janie <janiefaria315@gmail.com>
2025-10-07 23:43:40 +0600

    Add files via upload

---
+++ Anton and Polyhedrons/Anton and Polyhedrons.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string shape;
+    int count=0;
+    for(int i=0;i<n;i++){
+        cin>>shape;
+        if(shape=="Tetrahedron"){
+            count=count+4;
+        }else if(shape=="Cube"){
+            count=count+6;
+        }else if(shape=="Octahedron"){
+            count=count+8;
+        }else if(shape=="Dodecahedron"){
+            count=count+12;
+        }else{
+            count=count+20;
+        }
+    }
+    cout<<count<<endl;
+}
