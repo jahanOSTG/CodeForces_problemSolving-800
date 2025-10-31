f89700feb67ac624f5782416dd4ad618bd3ffda7
Faria Jahan Janie <janiefaria315@gmail.com>
2025-10-31 21:58:46 +0600

    Add files via upload

---
+++ A+B/A+B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    string s;
+    while(t--){
+        cin>>s;
+        int a=s[0]-'0';
+        int b=s[2]-'0';
+        cout<<a+b<<endl;
+    }
+}
